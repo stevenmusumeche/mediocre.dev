bfd56e0b6f754475db2bb1fbef86caf7e0aa74e6
Steven Musumeche <steven@musumeche.com>
2021-01-25 18:09:02 -0600

    scaffolding

---
+++ src/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./home.css";
+
+function Home() {
+  return (
+    <article>
+      <main>the home of mediocre developers like you</main>
+      <footer>
+        created by <a href="https://musumeche.com">Steven Musumeche</a>
+      </footer>
+    </article>
+  );
+}
+
+export default Home;
